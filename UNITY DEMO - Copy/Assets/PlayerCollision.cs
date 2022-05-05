0d0534119aaccaa3920e62863bc3e98a8d85f688
Aron676 <102623778+Aron676@users.noreply.github.com>
2022-05-05 17:23:08 -0500

    GAme Project finished
    The game project with what i was missing

---
+++ UNITY DEMO - Copy/Assets/PlayerCollision.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  void OnCollisionEnter(Collision c)
+  {
+
+    if(Equals(c.gameObject.tag, "wall"))
+    {
+
+	Debug.Log("Player Hit a wall... What should we do?");
+
+	//Player loses HP.
+	//Level is restarted
+	//A fight has started
+    
+    }
+  }
+}
